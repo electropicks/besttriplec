92a235a34b220ce6934c204655961f984e6149f9
electropicks <lkbidy@gmail.com>
2023-10-24 14:40:12 -0700

    fix potion bottling

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -1,17 +1,27 @@
 import random
-from sqlalchemy import insert, select, update
+
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
-from src import database as db
+from sqlalchemy import func
+from sqlalchemy.orm import Session
+
 from src.api import auth
 from src.api.audit import get_inventory
-from src.database import global_catalog, global_inventory
+from src.database import SessionLocal, Base
 
 router = APIRouter(
     prefix="/bottler",
     tags=["bottler"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 # Assuming that your database's metadata is defined in your actual application
@@ -21,67 +31,69 @@ class PotionInventory(BaseModel):
 
 
 @router.post("/deliver")
-def post_deliver_bottles(potions_delivered: list[PotionInventory]):
-    """Receive deliveries of new potions and update the inventory."""
-    print("potions delivered:", potions_delivered)
+async def post_deliver_bottles(potions_delivered: list[PotionInventory], db: Session = Depends(get_db)):
+    """Receive deliveries of new potions, update the inventory, global catalog, and ledgers."""
+
+    # Set a seller ID based on your business logic. For this example, it's a fixed value.
+    seller_id = "besttriplec"
+
+    # Retrieve the seller's global inventory
+    global_inventory = db.query(GlobalInventory).filter_by(seller=seller_id).first()
+
+    if not global_inventory:
+        raise HTTPException(status_code=400, detail="No inventory record found for the seller.")
+
     for potion in potions_delivered:
-        red_ml = potion.potion_type[0]
-        green_ml = potion.potion_type[1]
-        blue_ml = potion.potion_type[2]
-        dark_ml = potion.potion_type[3]
-
-        sku = f"r{red_ml}g{green_ml}b{blue_ml}d{dark_ml}"
-
+        # Extract the potion details
+        red_ml, green_ml, blue_ml, dark_ml = potion.potion_type
         quantity = potion.quantity
-        print("Current potion, sku, quantity:", potion, sku, quantity)
 
-        with db.engine.begin() as connection:
-            # Check if the potion already exists in the catalog
-            get_potion_stmt = select([global_catalog]).where(global_catalog.c.sku == sku)
-            result = connection.execute(get_potion_stmt).fetchall()
-            print("Executed get_potion_sql")
+        # Check if this potion type already exists in the global catalog
+        existing_potion = db.query(GlobalCatalog).filter(
+            (GlobalCatalog.red_ml == red_ml) &
+            (GlobalCatalog.green_ml == green_ml) &
+            (GlobalCatalog.blue_ml == blue_ml) &
+            (GlobalCatalog.dark_ml == dark_ml)
+        ).first()
 
-            if len(result) == 0:
-                # Potion doesn't exist, insert a new record
-                add_potion_stmt = insert(global_catalog).values(
-                    sku=sku,
-                    red_ml=red_ml,
-                    green_ml=green_ml,
-                    blue_ml=blue_ml,
-                    dark_ml=dark_ml,
-                    quantity=quantity
-                )
-                connection.execute(add_potion_stmt)
-                print("Executed add_potion_sql")
-            else:
-                # Potion exists, update the quantity
-                update_potion_stmt = (
-                    update(global_catalog)
-                    .where(global_catalog.c.sku == sku)
-                    .values(quantity=global_catalog.c.quantity + quantity)
-                )
-                connection.execute(update_potion_stmt)
-                print("Executed update_potion_sql")
-
-            # Update the inventory
-            update_inventory_stmt = (
-                update(global_inventory)
-                .values(
-                    red_ml=global_inventory.c.red_ml - red_ml,
-                    green_ml=global_inventory.c.green_ml - green_ml,
-                    blue_ml=global_inventory.c.blue_ml - blue_ml,
-                    dark_ml=global_inventory.c.dark_ml - dark_ml
-                )
+        if existing_potion:
+            # The potion type exists; we need to increment the quantity in the global catalog.
+            existing_potion.quantity += quantity
+            potion_id_for_ledger = existing_potion.potion_id  # To be used in the potion ledger entry.
+        else:
+            # The potion type does not exist; we need to create a new record in the global catalog.
+            new_potion = GlobalCatalog(
+                red_ml=red_ml,
+                green_ml=green_ml,
+                blue_ml=blue_ml,
+                dark_ml=dark_ml,
+                quantity=quantity,
+                sku=f"POT-{red_ml}-{green_ml}-{blue_ml}-{dark_ml}"
             )
-            connection.execute(update_inventory_stmt)
-            print("Executed update_inventory_sql")
+            db.add(new_potion)
+            db.flush()  # To get the new_potion.potion_id populated.
+            potion_id_for_ledger = new_potion.potion_id
 
-    return "OK"
+        # Record the transaction in the potion ledger
+        potion_ledger_entry = PotionLedgerEntries(
+            potion_id=potion_id_for_ledger,
+            quantity_change=quantity,  # This assumes quantity is the change (i.e., new bottles delivered).
+            description=f"Received delivery of {quantity} potions.",
+            potion_sku=existing_potion.sku if existing_potion else new_potion.sku
+        )
+        db.add(potion_ledger_entry)
+
+        # ... [Code for updating the global inventory and inventory ledger as previously described] ...
+
+    # Commit the session to save all changes
+    db.commit()
+
+    return {"message": "Delivery processed and inventories updated successfully."}
 
 
 # Gets called 4 times a day
 @router.post("/plan")
-def get_bottle_plan():
+async def get_bottle_plan():
     """
     Create a potion mix with random components ensuring the total is always 100ml.
     """
@@ -149,3 +161,5 @@ def get_bottle_plan():
 
 InventoryLedgerEntries = Base.classes.inventory_ledger_entries
 GlobalCatalog = Base.classes.global_catalog
+GlobalInventory = Base.classes.global_inventory
+PotionLedgerEntries = Base.classes.potion_ledger_entries
