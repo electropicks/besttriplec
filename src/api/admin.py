70722a2593e66aefe473f7bab05fd0e6437fef73
electropicks <lkbidy@gmail.com>
2023-10-31 01:06:38 -0700

    fix reset (sort of)

--- src/api/admin.py
+++ src/api/admin.py
@@ -5,6 +5,8 @@ from src.api import auth
 from src import database as db
 import sqlalchemy
 
+from src.database import GlobalInventory, GlobalCatalog, GlobalCarts, InventoryLedgerEntries
+
 router = APIRouter(
     prefix="/admin",
     tags=["admin"],
@@ -17,7 +19,7 @@ def reset(db: Session = Depends(db.get_db)):
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
-    db.query(db.GlobalInventory).update(
+    db.query(GlobalInventory).update(
         {
             "red_ml": 0,
             "green_ml": 0,
@@ -26,28 +28,9 @@ def reset(db: Session = Depends(db.get_db)):
             "checking_gold": 100,
             "saving_gold": 0,
         })
-
-    clear_inventory_sql = sqlalchemy.text(
-        "update global_inventory set \
-         red_ml = 0, \
-         green_ml = 0, \
-         blue_ml = 0, \
-         dark_ml = 0, \
-         checking_gold = 100, \
-         saving_gold = 0"
-    )
-    print("clear_inventory_sql: ", clear_inventory_sql)
-    clear_potions_sql = sqlalchemy.text("delete from global_catalog")
-    print("clear_potions_sql: ", clear_potions_sql)
-    clear_carts_sql = sqlalchemy.text("delete from global_carts")
-    print("clear_carts_sql: ", clear_carts_sql)
-    with db.engine.begin() as connection:
-        connection.execute(clear_inventory_sql)
-        print("Executed clear_inventory_sql")
-        connection.execute(clear_potions_sql)
-        print("Executed clear_potions_sql")
-        connection.execute(clear_carts_sql)
-        print("Executed clear_carts_sql")
+    db.query(GlobalCatalog).delete()
+    db.query(GlobalCarts).delete()
+    db.commit()
     return "OK"
 
 
